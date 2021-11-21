6b2eb03c822b4cecebebe107a7b8b7a2ab78c61e
SSMZHN <ssmzhn@163.com>
2021-11-21 15:32:23 +0800

    First Version

---
+++ README.md
@@ -0,0 +1,35 @@
+> *Let everyone enjoy the fun of fucking*
+> 
+> -- Chi\_Tang
+
+[![State-of-the-art Shitcode](https://img.shields.io/static/v1?label=State-of-the-art&message=Shitcode&color=7B5804)](https://github.com/trekhleb/state-of-the-art-shitcode)
+
+# FuckComputer-Python
+This is a **GREAT** program to fuck your computer!    
+It mixed other **GREAT** programs by FuckComputer together.
+For more information, see [FuckComputer]('https://github.com/FuckComputer').
+
+## Run
+### Dependencies
+- Python 3
+- Dependencies of other programs by FuckComputer
+
+### Linux & macOS & Android Termux
+Just run
+
+```shell
+python src/main.py
+```
+
+And it will work!
+
+### Windows
+Not supported for Windows yet.
+
+> *Windows is a piece of SHIT!*
+> -- Chi\_Tang
+## Usage
+WIP
+
+## License
+GPL-3.0
---
+++ src/main.py
@@ -0,0 +1,35 @@
+#Let everyone enjoy the fun of fucking. --Chi_Tang
+import requests, json, os
+print('Let everyone enjoy the fun of fucking.')
+try:
+    print('Checking Internet...')
+    js=requests.get('https://ssmzhn.github.io/FuckComputer.json')
+    print('Checking git...')
+    if os.system('git --version')!=0:
+        print('Git is not found!')
+        sys.exit(255)
+except requests.exceptions.ConnectionError:
+    print('Great connection!')
+    sys.exit(255)
+
+print('Welcome to FuckComputer n in 1 (Python ver)!')
+ls=json.loads(js.text)
+for x in range(len(ls.keys())):
+    print('    ({}){}'.format(x+1,list(ls.keys())[x]))
+ch=int(input('Please choose a mode: '))
+choice=list(ls.keys())[ch-1]
+os.system('git clone https://github.com/FuckComputer/{}'.format(choice))
+if ls[choice]=='cpp':
+    if os.system('g++ --version')!=0:
+        print('G++ is not found!')
+        sys.exit(255)
+    os.system('g++ {}/src/main.cpp'.format(choice))
+    os.system('./a.out')
+elif ls[choice]=='nim':
+    if os.system('nim --version')!=0:
+        print('Nim is not found!')
+        sys.exit(255)
+    os.system('nim compile -d:release {}/src/main.nim'.format(choice))
+    os.system('{}/src/main'.format(choice))
+elif ls[choice]=='python':
+    os.system('python {}/src/main.py'.format(choice))
